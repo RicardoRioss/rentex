128f8d1c799ca95f35adda5278dcf5dc14c683d1
Ricardo Rios <rickrios23@hotmail.com>
2022-05-17 16:24:16 -0300

    .

---
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -0,0 +1,10 @@
+import { ImportCategoryController } from "./ImportCategoryController";
+import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
+
+const categoryRepository = null;
+const importCategoryUseCase = new ImportCategoryUseCase(categoryRepository);
+const importCategoryController = new ImportCategoryController(
+  importCategoryUseCase,
+);
+
+export { importCategoryController };
---
+++ src/modules/cars/useCases/listCategories/index.ts
@@ -0,0 +1,10 @@
+import { ListCategoriesController } from "./ListCategoriesController";
+import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
+
+const categoriesRepository = null;
+const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRepository);
+const listCategoriesController = new ListCategoriesController(
+  listCategoriesUseCase,
+);
+
+export { listCategoriesController, categoriesRepository };
